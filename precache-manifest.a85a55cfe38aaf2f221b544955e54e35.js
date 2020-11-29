7d75103009214d24079fc75db4dbd8aa583b2dfa
akida <47895118+Akida31@users.noreply.github.com>
2020-11-29 16:42:27 +0100

    Updates

---
+++ precache-manifest.a85a55cfe38aaf2f221b544955e54e35.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204b9153b5633e1a375dd1f12004c863",
+    "url": "/boardit/index.html"
+  },
+  {
+    "revision": "3999a9ee6481b03e8bd6",
+    "url": "/boardit/static/css/2.129c44da.chunk.css"
+  },
+  {
+    "revision": "2feae949deaeab2097bf",
+    "url": "/boardit/static/css/main.71dc07c8.chunk.css"
+  },
+  {
+    "revision": "3999a9ee6481b03e8bd6",
+    "url": "/boardit/static/js/2.84b7638a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boardit/static/js/2.84b7638a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2feae949deaeab2097bf",
+    "url": "/boardit/static/js/main.aa8ff03c.chunk.js"
+  },
+  {
+    "revision": "939f75518c0cbe3e0d6a",
+    "url": "/boardit/static/js/runtime-main.86688489.js"
+  },
+  {
+    "revision": "72217712eb8d28872e7069322f3fda23",
+    "url": "/boardit/static/media/source-sans-pro-italic.72217712.eot"
+  },
+  {
+    "revision": "8256cfd7e4017a7690814879409212cd",
+    "url": "/boardit/static/media/source-sans-pro-italic.8256cfd7.ttf"
+  },
+  {
+    "revision": "e74f0128884561828ce8c9cf5c284ab8",
+    "url": "/boardit/static/media/source-sans-pro-italic.e74f0128.woff"
+  },
+  {
+    "revision": "1d71438462d532b62b05cdd7e6d7197d",
+    "url": "/boardit/static/media/source-sans-pro-regular.1d714384.eot"
+  },
+  {
+    "revision": "2da39ecf9246383937da11b44b7bd9b4",
+    "url": "/boardit/static/media/source-sans-pro-regular.2da39ecf.ttf"
+  },
+  {
+    "revision": "e7acc589bb558fe58936a853f570193c",
+    "url": "/boardit/static/media/source-sans-pro-regular.e7acc589.woff"
+  },
+  {
+    "revision": "0f3da1edf1b5c6a94a6ad948a7664451",
+    "url": "/boardit/static/media/source-sans-pro-semibold.0f3da1ed.eot"
+  },
+  {
+    "revision": "1cb8e94f1185f1131a0c895165998f2b",
+    "url": "/boardit/static/media/source-sans-pro-semibold.1cb8e94f.woff"
+  },
+  {
+    "revision": "f3565095e6c9158140444970f5a2c5ed",
+    "url": "/boardit/static/media/source-sans-pro-semibold.f3565095.ttf"
+  },
+  {
+    "revision": "58153ac7194e141d1e73ea88c6b63861",
+    "url": "/boardit/static/media/source-sans-pro-semibolditalic.58153ac7.eot"
+  },
+  {
+    "revision": "6b058fc2634b01d837c3432316c3141f",
+    "url": "/boardit/static/media/source-sans-pro-semibolditalic.6b058fc2.woff"
+  },
+  {
+    "revision": "c7e698a4d0956f4a939f42a05685bbf5",
+    "url": "/boardit/static/media/source-sans-pro-semibolditalic.c7e698a4.ttf"
+  }
+]);
